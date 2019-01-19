e11942228ffd72edc401cee0a979cd622bf62647
Vinayak Sahal <32996115+vsahal@users.noreply.github.com>
2019-01-18 18:01:37 -0600

    Create revStr2.py
    reverses a string with consonants at front and vowels in the end

---
+++ revStr2.py
@@ -0,0 +1,12 @@
+def revStr(s):
+
+  setVowels = set('aeiou')
+  emptyStr = ''
+
+  for i in range(len(s) - 1, -1, -1):
+    if s[i] in setVowels:
+      emptyStr = emptyStr + s[i]
+    else:
+      emptyStr = s[i] + emptyStr
+
+  return emptyStr
